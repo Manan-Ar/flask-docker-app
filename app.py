ea9dcf78c4dd034c7c2727bb18e10494e4b976e7
Manan-Ar <manan1950a@gmail.com>
2026-04-18 10:18:33 +0000

    Updated app to test CI/CD pipeline

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Docker + Flask!"
+    return "Hello from CI/CD 🚀 (Docker + GitHub Actions working!)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-return "Hello from CI/CD 🚀"
